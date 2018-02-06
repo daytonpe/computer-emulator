f374c3e0395dea293ab66f3fcc1591670e085071
Pat Dayton <daytonpe14@gmail.com>
2018-02-06 17:33:43 -0600

    pipe int from cpu to memory

--- index.cpp
+++ index.cpp
@@ -11,17 +11,168 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <string.h>
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
 // #include <wait.h>
 
+int loadProgram(const char* file_name);
+
+
+
+int main(int argc, char** argv)
+{
+   //TODO ignore error checks for simplicity
+
+   //Grab the arguments from the CLI
+   string file_name = argv[1];
+   int timer = atoi(argv[2]);
+   cout<<"Program File:    "<<file_name<<'\n';
+   cout<<"Interrupt timer: "<<timer<<'\n';
+
+   //Create pipes
+   int cpu_to_mem[2];
+   int mem_to_cpu[2];
+   int result1 = pipe(cpu_to_mem);
+   int result2 = pipe(mem_to_cpu);
+   if (result1 == -1 || result2 == -1){//pipe failed
+     cout << "There has been an error in the pipe..." << '\n';
+     exit(1);
+   }
+
+   char buf[30];
+   int PC;
+
+   //pass txt file into cpu first
+   //while not exit (true), switch for the 30 commands
+   //cpu requests next instruction
+   //incrementing on the PC
+
+
+   //Fork int CPU and Memory.
+   int z = fork();
+
+   if (z<0){
+     cout << "There has been an error in the fork..." << '\n';
+     exit(-1);
+   }
+
+   // Memory Code
+   else if (z==0) { //child process code
+
+     //Create Memory Array
+     int mem[2000]; //2000 integer entries, 0-999 for the user program, 1000-1999 for system code
+
+     //Load txt file into memory //TODO: should this be in here? or outside?
+     loadProgram(file_name.c_str());
+
+     //Piped in data
+     char command = 'R';
+     int address = 3;
+     int data = 99;
+
+     int resp;
+
+     read(cpu_to_mem[0], &resp, sizeof(resp));
+     cout << "Memory read: "<<  resp <<endl;
+
+     // //read/write functionality
+     // switch (command) {
+     //   case 'R': //Read function
+     //      //return mem[address];
+     //      cout << "Read Called" << '\n';
+     //   break;
+     //   case 'W': //Write function
+     //      // mem[address] = data;
+     //      cout << "Write Called" << '\n';
+     //   break;
+     // }
+
+
+     _exit(0); //terminate this process
+
+   }
+
+   // CPU Code
+   else { //parent process code
+
+     //Create Registers
+     int PC, SP, IR, AC, X, Y;
+
+     PC = 1414; //test value;
+
+
+     write(cpu_to_mem[1], &PC, sizeof(PC));
+
+
+
+     cout << "CPU process was called" << '\n';
+   }
+
+
+
+
+
+//    int x, y, z;
+//    int pd1[2], pd2[2];
+//
+//    x = pipe(pd1);
+//    y = pipe(pd2);
+//
+//    z = fork();
+//
+//    if (z == 0)
+//    { // child
+//       int a=5;
+//       int b=10;
+//       char req;
+//
+//       read(pd1[0], &req, sizeof(req));
+//       while (req != 'X')
+//       {
+//     	  if (req == 'A')
+//     	     write(pd2[1], &a, sizeof(a));
+//     	  else if (req == 'B')
+//     	     write(pd2[1], &b, sizeof(b));
+//
+//     	  read(pd1[0], &req, sizeof(req));
+//       }
+//
+//       _exit(0);
+//    }
+//    else
+//    { // parent
+//       int resp;
+//       char req;
+//
+//       printf("Enter A or B (or X to exit): ");
+//       scanf(" %c", &req);
+//
+//       while (req != 'X')
+//       {
+//     	  write(pd1[1], &req, sizeof(req));
+//     	  read(pd2[0], &resp, sizeof(resp));
+//
+//     	  printf("The value of a is: %d\n", resp);
+//
+//     	  printf("Enter A or B (or X to exit): ");
+//     	  scanf(" %c", &req);
+//       }
+//
+//       write(pd1[1], &req, sizeof(req));
+//       waitpid(-1,NULL,0);
+//    }
+
+
+   return 0;
+}
 
 
 //Load program from txt FILE
-//base code from https://www.codingunit.com/c-tutorial-file-io-using-text-files
-// void read_ints (const char* file_name)
+//Originally wrote in C, converted rest of file to C++
 int loadProgram(const char* file_name)
 {
   FILE *ptr_file;
@@ -58,89 +209,18 @@ int loadProgram(const char* file_name)
           memory[line] = num;
         }
       }
+
 	fclose(ptr_file);
 
 
-  for(int j = 1; j < 2000; j++) {
-      printf("%d", j);
-      printf("%s", "\t");
-      printf("%d", memory[j]);
-      printf("%s", "\n");
-  }
+  // for(int j = 1; j < 2000; j++) {
+  //     printf("%d", j);
+  //     printf("%s", "\t");
+  //     printf("%d", memory[j]);
+  //     printf("%s", "\n");
+  // }
+
+  printf("%s", "\nPROGRAM LOADED...\n\n");
 
  	return 0;
-}
-
-int main(int argc, char** argv)
-{
-   //TODO ignore error checks for simplicity
-
-
-   //Create Registers
-   int PC, SP, IR, AC, X, Y;
-
-   //Create Memory Array
-   int mem[2000]; //2000 integer entries, 0-999 for the user program, 1000-1999 for system code
-
-   //Grab the arguments from the CLI
-   string file_name = argv[1];
-   int timer = atoi(argv[2]);
-   cout<<"Program File:    "<<file_name<<'\n';
-   cout<<"Interrupt timer: "<<timer<<'\n';
-
-   //Load txt file into memory
-   loadProgram(file_name.c_str());
-
-   // int x, y, z;
-   // int pd1[2], pd2[2];
-   //
-   // x = pipe(pd1);
-   // y = pipe(pd2);
-   //
-   // z = fork();
-   //
-   // if (z == 0)
-   // { // child
-   //    int a=5;
-   //    int b=10;
-   //    char req;
-   //
-   //    read(pd1[0], &req, sizeof(req));
-   //    while (req != 'X')
-   //    {
-   //  	  if (req == 'A')
-   //  	     write(pd2[1], &a, sizeof(a));
-   //  	  else if (req == 'B')
-   //  	     write(pd2[1], &b, sizeof(b));
-   //
-   //  	  read(pd1[0], &req, sizeof(req));
-   //    }
-   //
-   //    _exit(0);
-   // }
-   // else
-   // { // parent
-   //    int resp;
-   //    char req;
-   //
-   //    printf("Enter A or B (or X to exit): ");
-   //    scanf(" %c", &req);
-   //
-   //    while (req != 'X')
-   //    {
-   //  	  write(pd1[1], &req, sizeof(req));
-   //  	  read(pd2[0], &resp, sizeof(resp));
-   //
-   //  	  printf("The value of a is: %d\n", resp);
-   //
-   //  	  printf("Enter A or B (or X to exit): ");
-   //  	  scanf(" %c", &req);
-   //    }
-   //
-   //    write(pd1[1], &req, sizeof(req));
-   //    waitpid(-1,NULL,0);
-   // }
-
-
-   return 0;
 }
